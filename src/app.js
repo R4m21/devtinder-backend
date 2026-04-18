69674c48d2c7f000bd926dcaf9e0709763a7ad89
mani <cr4m21@gmail.com>
2026-04-19 03:55:36 +0530

    feat: create a server using express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
-console.log("starting a new project!!!");
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 7777;
+
+// app.use((req, res) => {
+//   // request handler function its handle all request
+//   res.send("hello from the server!");
+// });
+
+app.use("/test", (req, res) => {
+  // request handler function its handle only /test request
+  res.send("hello from the server test!");
+});
+
+app.use("/hello", (req, res) => {
+  // request handler function its handle only /hello request
+  res.send("hello hello server hello!");
+});
+
+app.use("/", (req, res) => {
+  // request handler function its handle only / request (home) || its work with first match request path and return to response
+  res.send("hello from the server home!");
+});
+
+app.listen(PORT, () => {
+  console.log(`server successfully listening on port ${PORT}...`);
+});
