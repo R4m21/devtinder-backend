f27f9ea1558c2297899361d1ca994993ab1be728
mani <cr4m21@gmail.com>
2026-04-19 17:19:10 +0530

    feat: writing middleware and routes 2

--- src/app.js
+++ src/app.js
@@ -7,7 +7,14 @@ const PORT = process.env.PORT || 7777;
 
 app.use("/user", (req, res, next) => {
   console.log("response user");
-  next();
+  const token = "xyz";
+  const isAuthorized = token === "xyz";
+  console.log("get data for user");
+  if (isAuthorized) {
+    next();
+  } else {
+    res.status(401).send("unauthorized");
+  }
 });
 
 // same route hai to ham ek route ke next() call kr ke second joki same route hai to vahi pe call transfer kr sakte means jese first route handle response nhi dena chahta hai to vo next() call krke uske same exect match route ko pass krte hai call ise hi middleware kahte hai joki bich me request ko intercept krta hai aur modified kr sakta hai aur next fucntion call kr sakta hai
@@ -17,14 +24,25 @@ app.use("/user", (req, res) => {
 });
 
 app.get("/user/getData", (req, res) => {
-  console.log("get data for user");
-  res.send("get data for user");
+  // const token = "xyz";
+  // const isAuthorized = token === "xyz";
+  // console.log("get data for user");
+  // if (isAuthorized) {
+    res.send("get data for user");
+  // } else {
+  //   res.status(401).send("unauthorized");
+  // }
 });
 
 app.use("/admin", (req, res, next) => {
-  console.log("response admin");
-  res.send("get data for admin");
-  // next();
+  const token = "xyzd";
+  const isAuthorized = token === "xyz";
+  console.log("admin response");
+  if (isAuthorized) {
+    res.send("get data for admin");
+  } else {
+    res.status(401).send("unauthorized");
+  }
 });
 
 app.listen(PORT, () => {
