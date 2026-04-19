d5040a55b7f84370108c8a38cf4acf029bb92043
mani <cr4m21@gmail.com>
2026-04-19 18:04:43 +0530

    feat: handling error and wild matching middleware handling error

--- src/app.js
+++ src/app.js
@@ -3,26 +3,30 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 7777;
 
-const { adminAuth, userAuth } = require("./middlewares/auth");
-
-// app.use("/user", userAuth); // its middleware for separate route then not use its use direct middleware there need that place like this:=> app.get("/user/getData", userAuth, (req, res) => {}
-// app.post("/user/login", (req, res) => {} and here login its not userAuth to bound
-
-app.use("admin", adminAuth); // its common middleware for all route for start with /admin
-
-app.post("/user/login", (req, res) => {
-  console.log("user login successfully");
-  res.send("user login successfully");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("something went wrong");
+  }
 });
 
-app.get("/user/getData", userAuth, (req, res) => {
-  console.log("get user data");
-  res.send("get user data");
+app.use("/user/data", (req, res) => {
+  // try {
+    // db logic here
+    throw new Error("hdchchhbc");
+    // res.send("get data")
+  // } catch (error) {
+    // res.status(500).send("something is wrong in user data");
+  // }
 });
 
-app.get("/admin/getData", (req, res) => {
-  console.log("get admin data");
-  res.send("get user data");
+// wild card match routes its handle all routes if any error is occurred its order its maintained its priority, yto isjo kabhi last me likhne ka koi bhi error ayega to ye catch karega because javacript and express ye line by line or route match krta hai, if ham iske top pe likhe ke to vaha pe error nhi miegga koi route se error aya aur last me handle nhi kiya hai user ko  pura error log milega joki understanding nhi hoga.
+// express js dynamic parameter leta hai jese 2 params hai to req, res, 3 params hai to req, res, next, 4 parmas hai to err, req, res, next. its sequence matter krta hai.
+
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    // log error
+    res.status(500).send("something went wrong");
+  }
 });
 
 app.listen(PORT, () => {
