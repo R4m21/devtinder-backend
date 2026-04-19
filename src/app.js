1d7e6ead43598d6b6e2c99c1c6090cd7406a58f4
mani <cr4m21@gmail.com>
2026-04-19 06:44:54 +0530

    feat: playing multiple route handler

--- src/app.js
+++ src/app.js
@@ -3,31 +3,40 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 7777;
 
-// we can use regex here
-app.get(/.*abc\/(\d+)\/(\w+)/, (req, res) => {
-  // http://localhost:7777/helloabc/123/mani?userName=mani&userId=123
-  console.log("req.path", req.path); // console.log -> /helloabc/123/mani
-  console.log("req.query", req.query); // console.log -> { userName: 'mani', userId: '123' }
-  console.log("req.params", req.params); // console.log -> { '0': '123', '1': 'mani' }
-  res.send({ firstName: "mani", lastName: "ram" });
-});
+// app.use(route, route handler function1,...route handler functionN);
+// app.use(route, rH1, rH2, rH3, rH4, rH5);
+// app.use(route, [rH1, rH2, rH3, rH4, rH5]);
+// app.use(route, rH1, rH2, [rH3, rH4], rH5);
+// all are gives same response
 
-// query parameter and its get value by req.query
-app.get("/user", (req, res) => {
-  console.log("req", req.query); // console.log -> { userName: 'mani', userId: '123' }
-  res.send({ firstName: "mani", lastName: "ram" });
-});
-
-// dynamic routing and its get value by req.params
-app.get("/user/:userId", (req, res) => {
-  console.log(req.params); // console.log -> { userId: '123' }
-  res.send({ firstName: "mani", lastName: "ram" });
-});
-
-app.get("/user/:userId/:name", (req, res) => {
-  console.log(req.params); // console.log -> { userId: '123', name: 'mani' }
-  res.send({ firstName: "mani", lastName: "ram" });
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("response 1");
+    // res.send("response 1");
+    next(); // its throwing error because server already send response and its again send the response then connection already closed
+  },
+  (req, res, next) => {
+    console.log("response 2");
+    // res.send("response 2");
+    next();
+  },
+  (req, res, next) => {
+    console.log("response 3");
+    // res.send("response 3");
+    next();
+  },
+  (req, res, next) => {
+    console.log("response 4");
+    // res.send("response 4");
+    next();
+  },
+  (req, res, next) => {
+    console.log("response 5");
+    res.send("response 5");
+    next(); // its throwing error because server need next handler function, so its showing error :-> Cannot GET /user
+  },
+);
 
 app.listen(PORT, () => {
   console.log(`server successfully listening on port ${PORT}...`);
