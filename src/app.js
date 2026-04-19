4910b5c9ff288dea332bf0b72e3977b398279d95
mani <cr4m21@gmail.com>
2026-04-19 17:35:15 +0530

    feat: writing auth middleware and routes

--- src/app.js
+++ src/app.js
@@ -5,10 +5,17 @@ const PORT = process.env.PORT || 7777;
 
 const { adminAuth, userAuth } = require("./middlewares/auth");
 
-app.use("/user", userAuth);
-app.use("admin", adminAuth);
+// app.use("/user", userAuth); // its middleware for separate route then not use its use direct middleware there need that place like this:=> app.get("/user/getData", userAuth, (req, res) => {}
+// app.post("/user/login", (req, res) => {} and here login its not userAuth to bound
 
-app.get("/user/getData", (req, res) => {
+app.use("admin", adminAuth); // its common middleware for all route for start with /admin
+
+app.post("/user/login", (req, res) => {
+  console.log("user login successfully");
+  res.send("user login successfully");
+});
+
+app.get("/user/getData", userAuth, (req, res) => {
   console.log("get user data");
   res.send("get user data");
 });
