87b4b70ea1c0aaf47e116eced48861d06c95ef33
mani <cr4m21@gmail.com>
2026-04-20 03:27:37 +0530

    feat: added and connected database

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 require("./config/env");
 const express = require("express");
 
+const { connectDB } = require("./config/database");
+
 const app = express();
 
 const PORT = process.env.PORT || 7777;
 
-app.listen(PORT, () => {
-  console.log(`server successfully listening on port ${PORT}...`);
-});
+connectDB()
+  .then(() => {
+    console.log("database is connected...");
+    app.listen(PORT, () => {
+      console.log(`server successfully listening on port ${PORT}...`);
+    });
+  })
+  .catch((err) => {
+    console.log("database cannot be connected!!!");
+  });
