c22042659c133e39948c15696757e253ef6507a7
mani <cr4m21@gmail.com>
2026-04-22 03:50:26 +0530

    feat: added profile change password api in profile router

--- src/routers/profile.js
+++ src/routers/profile.js
@@ -2,11 +2,16 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const { validateEditProfileData } = require("../utils/validation");
 const User = require("../models/user");
+const isStrongPassword = require("validator/lib/isStrongPassword");
 const profileRouter = express.Router();
 
 profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
-    return res.send(req.user);
+    const loggedInUser = req.user;
+    return res.json({
+      message: `hello ${loggedInUser.firstName}`,
+      data: loggedInUser,
+    });
   } catch (err) {
     return res.status(400).send("Error : " + err.message);
   }
@@ -16,21 +21,31 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
   try {
     validateEditProfileData(req);
 
-    // // 1. findByIdAndUpdate (Direct DB Update)
-    // const user = await User.findByIdAndUpdate(req.user._id, req.body, {
-    //   returnDocument: "after",
-    //   runValidators: true,
-    // });
-    // return res.send(user);
-
-    // 2. loggedInUser.save() (Document-Based Update) its most recommended for validation and its already fetch data from DB
     const loggedInUser = req.user;
     Object.keys(req.body).forEach(
       (field) => (loggedInUser[field] = req.body[field]),
     );
     await loggedInUser.save();
+
     return res.json({
       message: `${loggedInUser.firstName}, your profile is updates successfully...`,
+      data: loggedInUser,
+    });
+  } catch (err) {
+    return res.status(400).send("Error : " + err.message);
+  }
+});
+
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+  try {
+    const { password } = req.body;
+    if (!isStrongPassword(password)) throw new Error("Enter a strong password");
+    const loggedInUser = req.user;
+    loggedInUser.password = password;
+    await loggedInUser.setPasswordHashInDB();
+    await loggedInUser.save();
+    return res.json({
+      message: "password update successfully...",
       data: loggedInUser,
     });
   } catch (err) {
