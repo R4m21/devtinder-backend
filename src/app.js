668e563b8863ed57d6bdcb0acc28b660baaef630
mani <cr4m21@gmail.com>
2026-05-02 01:42:38 +0530

    chore: env setting and  server error logs

--- src/app.js
+++ src/app.js
@@ -47,13 +47,18 @@ app.use((err, req, res, next) => {
   });
 });
 
-connectDB()
-  .then(() => {
-    console.log("Database connected successfully...");
+const startServer = async () => {
+  try {
+    await connectDB();
+    console.log("Database connected successfully");
+
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+      console.log(`Server running on port ${PORT}`);
     });
-  })
-  .catch((err) => {
-    console.error("Database connection failed:", err.message);
-  });
+  } catch (err) {
+    console.error("Database connection failed:", err);
+    process.exit(1);
+  }
+};
+
+startServer();
--- src/config/env.js
+++ src/config/env.js
@@ -1,3 +1,4 @@
 const dotenv = require("dotenv");
-const envFile = `.env.${process.env.NODE_ENV || "development"}`;
-dotenv.config({ path: envFile });
+// const envFile = `.env.${process.env.NODE_ENV || "development"}`;
+// dotenv.config({ path: envFile });
+dotenv.config();
